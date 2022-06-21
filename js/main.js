c201a72398b8c7045755489cd7cb817315bd3d30
edwinfonseca <71416287+edwinfonseca@users.noreply.github.com>
2022-06-20 19:31:20 -0500

    fix:add study

--- js/main.js
+++ js/main.js
@@ -1,111 +1,113 @@
-    // var cont=0;
-    var read_ingles=0, lire_frances=0, leer_espanol=0;
-    var write_ingles=0, ecrire_frances=0, escribir_espanol=0;
-    var speak_ingles=0, parler_frances=0, hablar_espanol=0;
+   
+    // var read_ingles=0, lire_frances=0, leer_espanol=0;
+    // var write_ingles=0, ecrire_frances=0, escribir_espanol=0;
+    // var speak_ingles=0, parler_frances=0, hablar_espanol=0;
     
 
-    // function barras(val,barr){
-    //     setInterval(function(){
-    //         if(cont<val){
-    //             cont++;
-    //             var barra= barr;
-    //             console.log(barr);
-    //             barra.addAttribute('style', 'width:'+cont+'%;');
-                
-    //             // barra.style.width=cont+'%';
-    //             // barra.innerHTML=cont+'%';
-    //         }
-    //     }), 1000;
-    // }
-
-    // barras(read_ingles, barr_read_ingles);
 
     //todo:---------------------- barras idiomas--------------------
+    // function barras(ini,barr,end){
+        
+    //     console.log(barr);
+    //     setInterval(function (){
+    //         if(ini<end){
+    //             ini++;
+    //             // var barr=document.querySelector("#barr-ingles-read");
+    //             // console.log(ini);
+    //             barr.style.width=ini+'%';
+    //             barr.innerHTML=ini+'%';
+    //         }
+    //     }), 1000;
+
+    // }
     
-    //*Ingles
-    setInterval(function(){
-        if(read_ingles<50){
-            read_ingles++;
-            var barra=document.querySelector("#barr-ingles-read");
-            //*alert(read_ingles);
-            barra.style.width=read_ingles+'%';
-            barra.innerHTML=read_ingles+'%';
-        }
-    }), 1000;
-    setInterval(function(){
-        if(write_ingles<50){
-            write_ingles++;
-            var barra=document.querySelector("#barr-ingles-write");
-            //*alert(write_ingles);
-            barra.style.width=write_ingles+'%';
-            barra.innerHTML=write_ingles+'%';
-        }
-    }), 1000;
-    setInterval(function(){
-        if(speak_ingles<50){
-            speak_ingles++;
-            var barra=document.querySelector("#barr-ingles-speak");
-            //*alert(speak_ingles);
-            barra.style.width=speak_ingles+'%';
-            barra.innerHTML=speak_ingles+'%';
-        }
-    }), 1000;
-    //*Frances
-    setInterval(function(){
-        if(lire_frances<50){
-            lire_frances++;
-            var barra=document.querySelector("#barr-frances-lire");
-            //*alert(lire_frances);
-            barra.style.width=lire_frances+'%';
-            barra.innerHTML=lire_frances+'%';
-        }
-    }), 1000;
-    setInterval(function(){
-        if(ecrire_frances<50){
-            ecrire_frances++;
-            var barra=document.querySelector("#barr-frances-ecrire");
-            //*alert(ecrire_frances);
-            barra.style.width=ecrire_frances+'%';
-            barra.innerHTML=ecrire_frances+'%';
-        }
-    }), 1000;
-    setInterval(function(){
-        if(parler_frances<50){
-            parler_frances++;
-            var barra=document.querySelector("#barr-frances-parler");
-            //*alert(parler_frances);
-            barra.style.width=parler_frances+'%';
-            barra.innerHTML=parler_frances+'%';
-        }
-    }), 1000;
-    //*Español
-    setInterval(function(){
-        if(leer_espanol<100){
-            leer_espanol++;
-            var barra=document.querySelector("#barr-espanol-leer");
-            //*alert(leer_espanol);
-            barra.style.width=leer_espanol+'%';
-            barra.innerHTML=leer_espanol+'%';
-        }
-    }), 1000;
-    setInterval(function(){
-        if(escribir_espanol<100){
-            escribir_espanol++;
-            var barra=document.querySelector("#barr-espanol-escribir");
-            //*alert(escribir_espanol);
-            barra.style.width=escribir_espanol+'%';
-            barra.innerHTML=escribir_espanol+'%';
-        }
-    }), 1000;
-    setInterval(function(){
-        if(hablar_espanol<100){
-            hablar_espanol++;
-            var barra=document.querySelector("#barr-espanol-hablar");
-            //*alert(hablar_espanol);
-            barra.style.width=hablar_espanol+'%';
-            barra.innerHTML=hablar_espanol+'%';
-        }
-    }), 1000;
+    // barras(cont, barra, fin);
+
+    
+    // //*Ingles
+    // setInterval(function(){
+    //     if(read_ingles<50){
+    //         read_ingles++;
+    //         var barra=document.querySelector("#barr-ingles-read");
+    //         //*alert(read_ingles);
+    //         barra.style.width=read_ingles+'%';
+    //         barra.innerHTML=read_ingles+'%';
+    //     }
+    // }), 1000;
+    // setInterval(function(){
+    //     if(write_ingles<50){
+    //         write_ingles++;
+    //         var barra=document.querySelector("#barr-ingles-write");
+    //         //*alert(write_ingles);
+    //         barra.style.width=write_ingles+'%';
+    //         barra.innerHTML=write_ingles+'%';
+    //     }
+    // }), 1000;
+    // setInterval(function(){
+    //     if(speak_ingles<50){
+    //         speak_ingles++;
+    //         var barra=document.querySelector("#barr-ingles-speak");
+    //         //*alert(speak_ingles);
+    //         barra.style.width=speak_ingles+'%';
+    //         barra.innerHTML=speak_ingles+'%';
+    //     }
+    // }), 1000;
+    // //*Frances
+    // setInterval(function(){
+    //     if(lire_frances<50){
+    //         lire_frances++;
+    //         var barra=document.querySelector("#barr-frances-lire");
+    //         //*alert(lire_frances);
+    //         barra.style.width=lire_frances+'%';
+    //         barra.innerHTML=lire_frances+'%';
+    //     }
+    // }), 1000;
+    // setInterval(function(){
+    //     if(ecrire_frances<50){
+    //         ecrire_frances++;
+    //         var barra=document.querySelector("#barr-frances-ecrire");
+    //         //*alert(ecrire_frances);
+    //         barra.style.width=ecrire_frances+'%';
+    //         barra.innerHTML=ecrire_frances+'%';
+    //     }
+    // }), 1000;
+    // setInterval(function(){
+    //     if(parler_frances<50){
+    //         parler_frances++;
+    //         var barra=document.querySelector("#barr-frances-parler");
+    //         //*alert(parler_frances);
+    //         barra.style.width=parler_frances+'%';
+    //         barra.innerHTML=parler_frances+'%';
+    //     }
+    // }), 1000;
+    // //*Español
+    // setInterval(function(){
+    //     if(leer_espanol<100){
+    //         leer_espanol++;
+    //         var barra=document.querySelector("#barr-espanol-leer");
+    //         //*alert(leer_espanol);
+    //         barra.style.width=leer_espanol+'%';
+    //         barra.innerHTML=leer_espanol+'%';
+    //     }
+    // }), 1000;
+    // setInterval(function(){
+    //     if(escribir_espanol<100){
+    //         escribir_espanol++;
+    //         var barra=document.querySelector("#barr-espanol-escribir");
+    //         //*alert(escribir_espanol);
+    //         barra.style.width=escribir_espanol+'%';
+    //         barra.innerHTML=escribir_espanol+'%';
+    //     }
+    // }), 1000;
+    // setInterval(function(){
+    //     if(hablar_espanol<100){
+    //         hablar_espanol++;
+    //         var barra=document.querySelector("#barr-espanol-hablar");
+    //         //*alert(hablar_espanol);
+    //         barra.style.width=hablar_espanol+'%';
+    //         barra.innerHTML=hablar_espanol+'%';
+    //     }
+    // }), 1000;
     
 
     //todo:-------------------
